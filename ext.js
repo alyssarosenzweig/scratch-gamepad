d9d4e953a389d72ab081695ea3782c06cccfd428
bobbybee <j104@rocketmail.com>
2014-12-26 15:26:07 -0500

    Gamepad polling

--- ext.js
+++ ext.js
@@ -1,8 +1,34 @@
 (function(ext) {
+	ext.gamepadSupport = !!navigator.webkitGetGamepads || !!navigator.webkitGamepads;
+	ext.gamepad = null;
+
+	ext.tick = function() {
+		// poll status
+		if(!ext.gamepad) {
+			ext.gamepad = navigator.webkitGetGamepads && navigator.webkitGetGamepads()[0];
+			
+			if(ext.gamepad) {
+				console.log("Got it!");
+			}
+		}
+
+		window.webkitRequestAnimationFrame(ext.tick);
+	}
+
+	if(ext.gamepadSupport) {
+		window.webkitRequestAnimationFrame(ext.tick);
+	}
+
 	ext._shutdown = function() {};
 	
 	ext._getStatus = function() {
+		if(!ext.gamepadSupport) return { status: 1, msg: "Please use a recent version of Google Chrome"};
+		if(!ext.gamepad) return { status: 1, msg: "Please plug in a gamepad and press any button"};
 
+		return {
+			status: 2,
+			msg: "Good to go!"
+		};
 	}
 
 	var descriptor = {
