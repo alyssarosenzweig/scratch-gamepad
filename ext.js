ad48176fce89e2c50d033c3e9864384ea908fc7d
bobbybee <j104@rocketmail.com>
2014-12-26 15:10:00 -0500

    Boilerplate

---
+++ ext.js
@@ -0,0 +1,14 @@
+(function(ext) {
+	ext._shutdown = function() {};
+	
+	ext._getStatus = function() {
+
+	}
+
+	var descriptor = {
+		blocks: [
+		]
+	}
+
+	ScratchExtensions.register("Gamepad", descriptor, ext);
+})({});
