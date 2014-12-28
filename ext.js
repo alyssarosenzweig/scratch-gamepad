08e117f9b606113cf31ad54c1f361863ae3ec899
blob8108 <blob8108@gmail.com>
2014-12-28 13:49:11 +0000

    Fix formatting

--- ext.js
+++ ext.js
@@ -1,54 +1,63 @@
 (function(ext) {
-	ext.gamepadSupport = !!navigator.webkitGetGamepads || !!navigator.webkitGamepads;
-	ext.gamepad = null;
 
-	ext.tick = function() {
-		// poll status
-		ext.gamepad = navigator.webkitGetGamepads && navigator.webkitGetGamepads()[0];
-		window.webkitRequestAnimationFrame(ext.tick);
-	}
+  ext.gamepadSupport = (!!navigator.webkitGetGamepads ||
+                        !!navigator.webkitGamepads);
+  ext.gamepad = null;
 
-	if(ext.gamepadSupport) {
-		window.webkitRequestAnimationFrame(ext.tick);
-	}
+  ext.tick = function() {
+    // poll status
+    ext.gamepad = (navigator.webkitGetGamepads &&
+                   navigator.webkitGetGamepads()[0]);
+    window.webkitRequestAnimationFrame(ext.tick);
+  };
+  if (ext.gamepadSupport) window.webkitRequestAnimationFrame(ext.tick);
 
-	ext._shutdown = function() {};
-	
-	ext._getStatus = function() {
-		if(!ext.gamepadSupport) return { status: 1, msg: "Please use a recent version of Google Chrome"};
-		if(!ext.gamepad) return { status: 1, msg: "Please plug in a gamepad and press any button"};
+  ext._shutdown = function() {};
 
-		return {
-			status: 2,
-			msg: "Good to go!"
-		};
-	}
+  ext._getStatus = function() {
+    if (!ext.gamepadSupport) return {
+      status: 1,
+      msg: "Please use a recent version of Google Chrome",
+    };
 
-	ext.isReady = function() {
-		return !!ext.gamepad;
-	}
+    if (!ext.gamepad) return {
+      status: 1,
+      msg: "Please plug in a gamepad and press any button",
+    };
 
-	ext.getID = function() {
-		return ext.gamepad.id;
-	}
+    return {
+      status: 2,
+      msg: "Good to go!",
+    };
+  };
 
-	ext.getButton = function(btn) {
-		return ext.gamepad.buttons[btn];
-	}
+  ext.isReady = function() {
+    return !!ext.gamepad;
+  };
 
-	ext.getAxis = function(axis) {
-		return ext.gamepad.axes[axis];
-	}
+  ext.getID = function() {
+    return ext.gamepad.id;
+  };
 
-	var descriptor = {
-		blocks: [
-			["r", "ready?", "isReady"],
-			["-"],
-			["r", "gamepad ID", "getID"],
-			["r", "get button %n", "getButton", 0],
-			["r", "get axis %n", "getAxis", 0],
-		]
-	}
+  ext.getButton = function(btn) {
+    return ext.gamepad.buttons[btn];
+  };
 
-	ScratchExtensions.register("Gamepad", descriptor, ext);
+  ext.getAxis = function(axis) {
+    return ext.gamepad.axes[axis];
+  };
+
+  var descriptor = {
+    blocks: [
+      ["r", "ready?", "isReady"],
+    ["-"],
+    ["r", "gamepad ID", "getID"],
+    ["r", "get button %n", "getButton", 0],
+    ["r", "get axis %n", "getAxis", 0],
+    ]
+  };
+
+  ScratchExtensions.register("Gamepad", descriptor, ext);
+
 })({});
+
